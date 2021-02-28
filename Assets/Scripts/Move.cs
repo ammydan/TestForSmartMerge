55422edfc0952fefd701e6b5872c14ba48fd4619
creatorD <772644080@qq.com>
2021-02-28 23:28:35 +0800

    test3 add movement and expand the map and navigation

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -14,14 +14,15 @@ public class Move : MonoBehaviour
     void Start()
     {
         waypoints_index = 0;
-        CruisePosition();
     }
      private void CruisePosition(){
          
       //Debug.DrawLine(transform.position, nextpos, Color.red);
+        print(NMA.remainingDistance+" "+"cruisePostion!"+NMA.stoppingDistance);
         if (NMA.remainingDistance <= NMA.stoppingDistance)
         {
            //Debug.Log(NMA.remainingDistance + " : " + NMA.stoppingDistance);
+           print("change !!!"+waypoints.Length+" "+waypoints_index+" "+waypoints[waypoints_index].position+" "+transform.position);
             waypoints_index = (waypoints_index + 1) % waypoints.Length;
             NMA.SetDestination(waypoints[waypoints_index].position);
         }
@@ -30,6 +31,7 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CruisePosition();
         
     }
 }
