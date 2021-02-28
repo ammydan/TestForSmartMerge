97af87a1ad42ebfced5701492cecbeb71ebeee33
creatorD <772644080@qq.com>
2021-02-28 11:45:41 +0800

    test1

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+
+    public Transform[] waypoints;
+    public NavMeshAgent NMA;
+
+    private int waypoints_index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints_index = 0;
+        CruisePosition();
+    }
+     private void CruisePosition(){
+         
+      //Debug.DrawLine(transform.position, nextpos, Color.red);
+        if (NMA.remainingDistance <= NMA.stoppingDistance)
+        {
+           //Debug.Log(NMA.remainingDistance + " : " + NMA.stoppingDistance);
+            waypoints_index = (waypoints_index + 1) % waypoints.Length;
+            NMA.SetDestination(waypoints[waypoints_index].position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
